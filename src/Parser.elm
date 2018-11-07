ce7475f4b68e81568af3246f1ceebb19b8473958
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-11-07 21:25:55 +0100

    Reorder the functions

--- src/Parser.elm
+++ src/Parser.elm
@@ -30,35 +30,33 @@ parse input parser =
             Err err
 
 
+succeed : a -> Parser a
+succeed val state =
+    Ok ( state, val )
+
+
+fail : String -> Parser a
+fail str state =
+    Err str
+
+
 withError : String -> Parser a -> Parser a
 withError msg parser state =
     parser state
         |> Result.mapError (\_ -> msg)
 
 
-int : Parser Int
-int =
-    let
-        digit : Parser Char
-        digit =
-            oneOf (List.map char [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ])
-
-        parseInt : List Char -> State -> Result String ( State, Int )
-        parseInt digits newState =
-            String.fromList digits
-                |> String.toInt
-                |> Result.fromMaybe "invalid int"
-                |> Result.map (\x -> ( newState, x ))
-    in
-        oneOrMore digit
-            |> andThen (\digits -> parseInt digits)
-            |> withError "expected int"
+andThen : (a -> Parser b) -> Parser a -> Parser b
+andThen next parser state =
+    parser state
+        |> Result.andThen
+            (\( newState, val ) -> next val newState)
 
 
-oneOrMore : Parser a -> Parser (List a)
-oneOrMore parser =
+map : (a -> b) -> Parser a -> Parser b
+map f parser =
     parser
-        |> andThen (\val -> map ((::) val) (zeroOrMore parser))
+        |> andThen (\x -> succeed (f x))
 
 
 zeroOrMore : Parser a -> Parser (List a)
@@ -75,36 +73,10 @@ zeroOrMore parser state =
         Ok (agg [] state)
 
 
-succeed : a -> Parser a
-succeed val state =
-    Ok ( state, val )
-
-
-fail : String -> Parser a
-fail str state =
-    Err str
-
-
-char : Char -> Parser Char
-char chr state =
-    if peek 1 state == [ chr ] then
-        Ok ( advance 1 state, chr )
-    else
-        Err "expected char"
-
-
-string : String -> Parser String
-string str =
-    String.toList str
-        |> List.map char
-        |> sequence
-        |> map String.fromList
-
-
-map : (a -> b) -> Parser a -> Parser b
-map f parser =
+oneOrMore : Parser a -> Parser (List a)
+oneOrMore parser =
     parser
-        |> andThen (\x -> succeed (f x))
+        |> andThen (\val -> map ((::) val) (zeroOrMore parser))
 
 
 sequence : List (Parser a) -> Parser (List a)
@@ -116,20 +88,6 @@ sequence parsers =
         parser :: rest ->
             parser
                 |> andThen (\x -> map (\xs -> x :: xs) (sequence rest))
-
-
-anyChar : Parser Char
-anyChar state =
-    List.head state.remaining
-        |> Maybe.map (\chr -> ( advance 1 state, chr ))
-        |> Result.fromMaybe "expected any char"
-
-
-andThen : (a -> Parser b) -> Parser a -> Parser b
-andThen next parser state =
-    parser state
-        |> Result.andThen
-            (\( newState, val ) -> next val newState)
 
 
 oneOf : List (Parser a) -> Parser a
@@ -159,3 +117,45 @@ advance length state =
         | position = state.position + length
         , remaining = List.drop length state.remaining
     }
+
+
+anyChar : Parser Char
+anyChar state =
+    List.head state.remaining
+        |> Maybe.map (\chr -> ( advance 1 state, chr ))
+        |> Result.fromMaybe "expected any char"
+
+
+char : Char -> Parser Char
+char chr state =
+    if peek 1 state == [ chr ] then
+        Ok ( advance 1 state, chr )
+    else
+        Err "expected char"
+
+
+string : String -> Parser String
+string str =
+    String.toList str
+        |> List.map char
+        |> sequence
+        |> map String.fromList
+
+
+int : Parser Int
+int =
+    let
+        digit : Parser Char
+        digit =
+            oneOf (List.map char [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ])
+
+        parseInt : List Char -> State -> Result String ( State, Int )
+        parseInt digits newState =
+            String.fromList digits
+                |> String.toInt
+                |> Result.fromMaybe "invalid int"
+                |> Result.map (\x -> ( newState, x ))
+    in
+        oneOrMore digit
+            |> andThen (\digits -> parseInt digits)
+            |> withError "expected int"
