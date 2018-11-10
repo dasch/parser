bf77026bcd831bf82f66cc26866933a7d615b0b3
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-11-10 14:33:11 +0100

    Add partial Markdown parser

---
+++ tests/Examples/Markdown.elm
@@ -0,0 +1,49 @@
+module Examples.Markdown exposing (Document, parse)
+
+import Parser exposing (..)
+import Parser.Common exposing (..)
+
+
+type alias Document =
+    List Block
+
+
+type Block
+    = Paragraph (List Inline)
+
+
+type Inline
+    = Text String
+
+
+parse : String -> Result String Document
+parse input =
+    Parser.parse input document
+
+
+document : Parser Document
+document =
+    separatedBy (oneOrMore emptyLine) block
+        |> ignoring end
+
+
+block : Parser Block
+block =
+    oneOf [ paragraph ]
+
+
+paragraph : Parser Block
+paragraph =
+    oneOrMore (map Text nonEmptyLine)
+        |> map Paragraph
+
+
+emptyLine : Parser String
+emptyLine =
+    string "\n"
+
+
+nonEmptyLine : Parser String
+nonEmptyLine =
+    oneOrMore (anyCharExcept '\n')
+        |> map String.fromList
