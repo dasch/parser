48e76842feef699b6e32cdd7b91dc93f296c7512
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-11-16 15:59:46 +0100

    Test with a bigger TOML example

--- tests/TomlTests.elm
+++ tests/TomlTests.elm
@@ -10,7 +10,11 @@ import Time
 suite : Test
 suite =
     describe "Toml"
-        [ describe "integers"
+        [ test "example document" <|
+            \_ ->
+                parse example
+                    |> Expect.ok
+        , describe "integers"
             [ test "valid integers" <|
                 \_ ->
                     parseValue "42"
@@ -164,4 +168,42 @@ multilineString =
     """\"\"\"
 hello
 \"\"\"
+"""
+
+
+example =
+    """
+# This is a TOML document.
+
+title = "TOML Example"
+
+[owner]
+name = "Tom Preston-Werner"
+dob = 1979-05-27T07:32:00-08:00 # First class dates
+
+[database]
+server = "192.168.1.1"
+ports = [ 8001, 8001, 8002 ]
+connection_max = 5000
+enabled = true
+
+[servers]
+
+  # Indentation (tabs and/or spaces) is allowed but not required
+  [servers.alpha]
+  ip = "10.0.0.1"
+  dc = "eqdc10"
+
+  [servers.beta]
+  ip = "10.0.0.2"
+  dc = "eqdc10"
+
+[clients]
+data = [ ["gamma", "delta"], [1, 2] ]
+
+# Line breaks are OK when inside arrays
+hosts = [
+  "alpha",
+  "omega"
+]
 """
