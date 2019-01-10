9877cb30f8c47bae082d05107684e2f6bf27e052
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2019-01-10 13:15:01 +0100

    Add Markdown benchmark

---
+++ benchmarks/MarkdownBenchmark.elm
@@ -0,0 +1,35 @@
+module Main exposing (input, suite)
+
+import Benchmark exposing (..)
+import Benchmark.Runner exposing (BenchmarkProgram, program)
+import Examples.Markdown as Markdown
+
+
+input : String
+input =
+    """
+# This is an example of Markdown
+
+1. One
+2. Two
+3. Three
+
+You can have *different* kinds of __emphasis__!
+
+* And
+* Unordered
+* Lists!
+"""
+
+
+suite : Benchmark
+suite =
+    describe "Markdown"
+        [ benchmark "parse" <|
+            \_ -> Markdown.parse input
+        ]
+
+
+main : BenchmarkProgram
+main =
+    program suite
