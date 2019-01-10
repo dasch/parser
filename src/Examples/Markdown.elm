61a3dfeb662e715575fefd44b777300d056da23c
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2019-01-10 13:26:44 +0100

    Make Markdown example work

--- src/Examples/Markdown.elm
+++ src/Examples/Markdown.elm
@@ -1,4 +1,4 @@
-module Examples.Markdown exposing (Document, parse)
+module Examples.Markdown exposing (Block(..), Document, Inline(..), parse)
 
 import Parser exposing (..)
 import Parser.Common exposing (..)
@@ -10,10 +10,14 @@ type alias Document =
 
 type Block
     = Paragraph (List Inline)
+    | OrderedList (List Block)
+    | Heading Int (List Inline)
 
 
 type Inline
     = Text String
+    | Emphasis String
+    | StrongEmphasis String
 
 
 parse : String -> Result Error Document
@@ -23,19 +27,105 @@ parse input =
 
 document : Parser Document
 document =
-    separatedBy (oneOrMore emptyLine) block
-        |> ignore end
+    oneOrMore block
 
 
 block : Parser Block
 block =
-    oneOf [ paragraph ]
+    zeroOrMore emptyLine
+        |> followedBy (oneOf [ heading, orderedList, paragraph ])
+        |> inContext "block"
+
+
+surroundedBy : Parser a -> Parser b -> Parser c -> Parser c
+surroundedBy before after inner =
+    before
+        |> followedBy inner
+        |> andThen
+            (\value ->
+                after
+                    |> followedBy (succeed value)
+            )
+
+
+inline : Parser (List Inline)
+inline =
+    let
+        span : Parser Inline
+        span =
+            oneOf [ strongEmphasis, emphasis, text ]
+
+        plain : Parser String
+        plain =
+            oneOrMore (except (oneOf [ newline, char '*', char '_' ]))
+                |> stringWith
+
+        text : Parser Inline
+        text =
+            map Text plain
+
+        strongEmphasis : Parser Inline
+        strongEmphasis =
+            surroundedBy (string "__") (string "__") plain
+                |> map StrongEmphasis
+
+        emphasis : Parser Inline
+        emphasis =
+            surroundedBy (char '*') (char '*') plain
+                |> map Emphasis
+    in
+    into identity
+        |> grab (oneOrMore span)
+        |> ignore (maybe newline)
+        |> inContext "inline"
+
+
+heading : Parser Block
+heading =
+    let
+        headingForLevel : List Char -> Parser Block
+        headingForLevel chars =
+            let
+                level =
+                    List.length chars
+            in
+            if level == 0 then
+                fail "not a heading"
+
+            else
+                oneOrMore blank
+                    |> followedBy inline
+                    |> map (Heading level)
+    in
+    oneOrMore (char '#')
+        |> andThen headingForLevel
+        |> inContext "heading"
 
 
 paragraph : Parser Block
 paragraph =
-    oneOrMore (map Text nonEmptyLine)
+    oneOrMore inline
+        |> map List.concat
         |> map Paragraph
+        |> inContext "paragraph"
+
+
+orderedList : Parser Block
+orderedList =
+    let
+        marker =
+            int
+                |> followedBy (char '.')
+                |> followedBy (oneOrMore blank)
+
+        item : Parser Block
+        item =
+            marker
+                |> followedBy inline
+                |> map Paragraph
+    in
+    oneOrMore item
+        |> map OrderedList
 
 
 emptyLine : Parser String
---
+++ tests/MarkdownTests.elm
@@ -0,0 +1,60 @@
+module MarkdownTests exposing (suite)
+
+import Examples.Markdown exposing (..)
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+input : String
+input =
+    """
+# This is an example of Markdown
+
+1. One
+2. Two
+3. Three
+
+You can have *different* kinds of __emphasis__!
+
+* And
+* Unordered
+* Lists!
+"""
+
+
+suite : Test
+suite =
+    describe "Markdown"
+        [ describe "parse"
+            [ test "parses Markdown documents" <|
+                \_ ->
+                    let
+                        expected : Document
+                        expected =
+                            [ Heading 1 [ Text "This is an example of Markdown" ]
+                            , OrderedList
+                                [ Paragraph [ Text "One" ]
+                                , Paragraph [ Text "Two" ]
+                                , Paragraph [ Text "Three" ]
+                                ]
+                            , Paragraph
+                                [ Text "You can have "
+                                , Emphasis "different"
+                                , Text " kinds of "
+                                , StrongEmphasis "emphasis"
+                                , Text "!"
+                                ]
+                            ]
+                    in
+                    parse input
+                        |> Expect.equal (Ok expected)
+            ]
+        , test "inline" <|
+            \_ -> expectMarkdown [ Paragraph [ Text "hello ", Emphasis "world" ] ] "hello *world*"
+        , test "heading" <|
+            \_ -> expectMarkdown [ Heading 2 [ Text "hello" ] ] "## hello"
+        ]
+
+
+expectMarkdown expected string =
+    Expect.equal (Ok expected) (parse string)
